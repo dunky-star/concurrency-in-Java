80adaa08e12adcf81ec98e7c779a70746c68af54
dunky-star <dunkygeoffrey39@gmail.com>
2023-07-14 11:30:54 +0300

    Creating thread color

--- basic_threads/src/Main.java
+++ basic_threads/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("\nThreads construction in Java!");
+        System.out.println(ThreadColor.ANSI_BLUE + "\nThreads construction in Java!");
         Thread thread1 = new Thread1();
         // To invoke the run method in thread1.
         thread1.start();
@@ -9,7 +9,7 @@ public class Main {
         // Creating an anonymous thread.
         new Thread() {
             public void run(){
-                System.out.println("Hello from the anonymous class thread.");
+                System.out.println(ThreadColor.ANSI_GREEN + "Hello from the anonymous class thread.");
             }
         }.start();
 
