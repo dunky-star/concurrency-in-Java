982ca8c62059998063ace43020a7f478d5d7c275
dunky-star <dunkygeoffrey39@gmail.com>
2023-07-15 17:19:19 +0300

    Preventing a race condition by using synchronized

--- basic_threads/src/Countdown.java
+++ basic_threads/src/Countdown.java
@@ -1,13 +1,12 @@
 public class Countdown {
-
-    public void doCountdown() {
+    public synchronized void doCountdown() {
         String color = switch (Thread.currentThread().getName()) {
             case "Thread 1" -> ThreadColor.ANSI_CYAN;
             case "Thread 2" -> ThreadColor.ANSI_PURPLE;
             default -> ThreadColor.ANSI_GREEN;
         };
 
-        for(int i=10; i > 0; i--) {
+        for(int i =10; i > 0; i--) {
             System.out.println(color + Thread.currentThread().getName() + ": i =" + i);
         }
     }
