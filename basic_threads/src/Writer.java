03f2dde352194553798fdd5e976ec366bac0a16e
dunky-star <dunkygeoffrey39@gmail.com>
2023-07-17 09:49:02 +0300

    Deadlocks,-wait,-notify-and-notifyAll

---
+++ basic_threads/src/Writer.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+class Writer implements Runnable {
+    private final Message message;
+
+    public Writer(Message message) {
+        this.message = message;
+    }
+
+    public void run() {
+        String[] messages = {
+                "Humpty Dumpty sat on a wall",
+                "Humpty Dumpty had a great fall",
+                "All the king's horses and all the king's men",
+                "Couldn't put Humpty together again"
+        };
+
+        Random random = new Random();
+
+        for(int i=0; i<messages.length; i++) {
+            message.write(messages[i]);
+            try {
+                Thread.sleep(random.nextInt(2000));
+            } catch(InterruptedException ignored) {
+
+            }
+        }
+        message.write("Finished");
+    }
+}
