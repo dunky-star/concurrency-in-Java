36e38ad71a394d1fafe1eb5cae96a1fc5cd632e9
dunky-star <dunkygeoffrey39@gmail.com>
2023-07-17 12:49:01 +0300

    Java Util Concurrent package

--- basic_threads/src/MyProducer.java
+++ basic_threads/src/MyProducer.java
@@ -17,7 +17,9 @@ class MyProducer implements  Runnable {
         for(String num: nums) {
             try {
                 System.out.println(color + "Adding..." + num);
-                buffer.add(num);
+                synchronized(buffer) {
+                    buffer.add(num);
+                }
 
                 Thread.sleep(random.nextInt(1000));
             } catch(InterruptedException e) {
@@ -26,6 +28,8 @@ class MyProducer implements  Runnable {
         }
 
         System.out.println(color + "Adding EOF and exiting....");
-        buffer.add("EOF");
+        synchronized(buffer) {
+            buffer.add("EOF");
+        }
     }
 }
