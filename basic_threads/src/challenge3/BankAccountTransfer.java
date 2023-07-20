779f7e94ee8ecf66f36341ad64ac76f9ff1b0dad
dunky-star <dunkygeoffrey39@gmail.com>
2023-07-20 19:13:58 +0300

    Challenge 3 - Using tryLock to manage thread interference.

--- basic_threads/src/challenge3/BankAccountTransfer.java
+++ basic_threads/src/challenge3/BankAccountTransfer.java
@@ -5,60 +5,58 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class BankAccountTransfer {
-        private double balance;
-        private final String accountNumber;
-        private final Lock lock = new ReentrantLock();
+    private double balance;
+    private final Lock lock = new ReentrantLock();
 
-        public BankAccountTransfer(String accountNumber, double balance) {
-            this.accountNumber = accountNumber;
-            this.balance = balance;
+    public BankAccountTransfer(String accountNumber, double balance) {
+        this.balance = balance;
+    }
+
+    public boolean withdraw(double amount) {
+        if (lock.tryLock()) {
+            try {
+                // Simulate database access
+                Thread.sleep(100);
+            }
+            catch (InterruptedException ignored) {
+            }
+            balance -= amount;
+            System.out.printf("%s: Withdrew %f\n", Thread.currentThread().getName(), amount);
+            return true;
         }
+        return false;
+    }
 
-        public boolean withdraw(double amount) {
-            if (lock.tryLock()) {
-                try {
-                    // Simulate database access
-                    Thread.sleep(100);
-                }
-                catch (InterruptedException ignored) {
-                }
-                balance -= amount;
-                System.out.printf("%s: Withdrew %f\n", Thread.currentThread().getName(), amount);
+    public boolean deposit(double amount) {
+        if (lock.tryLock()) {
+            try {
+                // Simulate database access
+                Thread.sleep(100);
+            }
+            catch (InterruptedException ignored) {
+            }
+            balance += amount;
+            System.out.printf("%s: Deposited %f\n", Thread.currentThread().getName(), amount);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean transfer(BankAccountTransfer destinationAccount, double amount) {
+        if (withdraw(amount)) {
+            if (destinationAccount.deposit(amount)) {
                 return true;
             }
-            return false;
-        }
-
-        public boolean deposit(double amount) {
-            if (lock.tryLock()) {
-                try {
-                    // Simulate database access
-                    Thread.sleep(100);
-                }
-                catch (InterruptedException ignored) {
-                }
-                balance += amount;
-                System.out.printf("%s: Deposited %f\n", Thread.currentThread().getName(), amount);
-                return true;
+            else {
+                // The deposit failed. Refund the money back into the account.
+                System.out.printf("%s: Destination account busy. Refunding money\n",
+                        Thread.currentThread().getName());
+                deposit(amount);
             }
-            return false;
         }
 
-        public boolean transfer(BankAccountTransfer destinationAccount, double amount) {
-            if (withdraw(amount)) {
-                if (destinationAccount.deposit(amount)) {
-                    return true;
-                }
-                else {
-                    // The deposit failed. Refund the money back into the account.
-                    System.out.printf("%s: Destination account busy. Refunding money\n",
-                            Thread.currentThread().getName());
-                    deposit(amount);
-                }
-            }
-
-            return false;
-        }
+        return false;
+    }
 }
 
 
